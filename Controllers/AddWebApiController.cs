9b06f39bf2a48c512c5661e9791aa82252ead240
FANUEL NIGUSSIE <fanuelniguse19@gmial.com>
2022-07-18 18:06:32 +0300

    sample commit

---
+++ Controllers/AddWebApiController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AddWebTask.Models;
+using Newtonsoft.Json;
+namespace AddWebTask.Controllers
+{
+    public class AddWebApiController : ApiController
+    {
+        private AddWebModel dbContext = new AddWebModel();
+
+        // GET api/<controller>
+        public List<AddUserModel> Get()
+        {
+            List<Users> us = dbContext.Users.ToList<Users>();
+            List<AddUserModel> users = new List<AddUserModel>();
+             for(int i=0; i<us.Count; i++)
+            {
+                AddUserModel tmp = new AddUserModel();
+                tmp.user = us[i];
+                int x = us[i].Id;
+                tmp.expriences = dbContext.Expriences.Where(e => e.ownerId == x).ToList<Exprience>();
+                users.Add(tmp);
+            }
+            return users;
+            
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+            AddUserModel addedUser = new AddUserModel();
+
+            addedUser = JsonConvert.DeserializeObject<AddUserModel>(value);
+            // addedUser.dob = Convert.ToDateTime(addedUser.dob);
+            //addedUser.Id = 1;
+            dbContext.Users.Add(addedUser.user);
+            dbContext.SaveChanges();
+            //dbContext.Users.Add(addedUser);
+            for(int i=0; i< addedUser.expriences.Count; i++)
+            {
+                int x = dbContext.Users.Count();
+                List<Users> users = new List<Users>();
+                users = dbContext.Users.ToList<Users>();
+
+                addedUser.expriences[i].ownerId = users.Last().Id;
+                dbContext.Expriences.Add(addedUser.expriences[i]);
+            }
+
+            dbContext.SaveChanges();
+            //dbContext.addUser(addedUser);
+            
+        }
+        
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using System.Web.Mvc;
+using System.Net.Http;
+using AddWebTask.Models;
+
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+using System.Configuration;
+
+namespace AddWebTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public System.Web.Mvc.ActionResult Index()
+        {
+            return View();
+        }
+        public System.Web.Mvc.ActionResult Details()
+        {
+            return View("Detail");
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public System.Web.Mvc.ActionResult Detail()
+        {
+            if (Request.Form.Keys.Count > 0)
+            {
+                int id = int.Parse(Request.Form.Keys[0]);
+                AddUserModel user = new AddUserModel();
+                AddWebModel dbContext = new AddWebModel();
+                user.expriences= dbContext.Expriences.Where(u => u.ownerId == id ).ToList<Exprience>();
+                user.user= dbContext.Users.Where(u => u.Id == id).FirstOrDefault();
+                ViewBag.detailUser = user;
+
+                return RedirectToActionPermanent("Details");
+
+            }
+            return View("Detail");
+        }
+        public System.Web.Mvc.ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [System.Web.Mvc.HttpPost]
+        [System.Web.Mvc.ActionName("addUser")]
+        public System.Web.Mvc.ActionResult addUser()
+        {
+            if (Request.Form.Keys.Count > 0)
+            {
+                Users addedUser = new Users();
+                addedUser.Fname = Request.Form.Keys[0];
+                addedUser.Lname = Request.Form.Keys[1];
+                string apiUrl = ConfigurationSettings.AppSettings["apiUrl"];
+                AddUserModel newUser = new AddUserModel();
+                newUser.user = new Users();
+                newUser.user = addedUser;
+               
+           //     
+                if (TempData.Peek("sessionExp") != null)
+                {
+                    List<Exprience> expList = JsonConvert.DeserializeObject<List<Exprience>>(TempData.Peek("sessionExp").ToString());
+                    
+                    if (expList.Count > 0)
+                    {
+                        newUser.expriences = new List<Exprience>();
+                        newUser.expriences = expList;
+                        apiUrl = ConfigurationSettings.AppSettings["apiUrl"];
+             //           HttpClient client1 = new HttpClient();
+               //         HttpResponseMessage response1 = client.PostAsJsonAsync<String>(apiUrl, JsonConvert.SerializeObject(expList).ToString()).Result;
+                 //       if (response1.IsSuccessStatusCode)
+                   //     {
+                     //       response1b = true;
+                       // }
+                    }
+                }
+
+
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = client.PostAsJsonAsync<String>(apiUrl,JsonConvert.SerializeObject(newUser) ).Result;
+              
+                if (response.IsSuccessStatusCode )
+                {
+                    return View("About");
+                }
+
+            }
+            return View("About");
+        }   
+        [System.Web.Mvc.HttpPost]
+        [System.Web.Mvc.ActionName("addExperience")]
+       
+
+        public Microsoft.AspNetCore.Mvc.NoContentResult addExperience()            
+        {
+            if (Request.Form.Keys.Count > 3)
+            {
+
+                Exprience exp = new Exprience();
+                exp.company = Request.Form.Keys[0];
+                exp.position = Request.Form.Keys[1];
+                exp.FromDate =Request.Form.Keys[2];
+                exp.toDate = Request.Form.Keys[3];
+                
+                
+                List<Exprience> expList = new List<Exprience>();
+                
+                if (TempData.Peek("sessionExp") == null)
+                {
+                    expList.Add(exp);
+                    TempData["sessionExp"] = JsonConvert.SerializeObject(expList);
+                }
+                else
+                {
+                    expList = JsonConvert.DeserializeObject<List<Exprience>>(TempData.Peek("sessionExp").ToString());
+                    expList.Add(exp);
+                    TempData["sessionExp"] = JsonConvert.SerializeObject(expList);
+                }
+            }
+            
+            
+            
+            //expList.Add(exp);
+          //  TempData["tempExp"] = JsonConvert.SerializeObject(expList);
+            return new NoContentResult();
+        }
+        public System.Web.Mvc.ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            string apiUrl = ConfigurationSettings.AppSettings["apiUrl"];
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(apiUrl).Result;
+            List<AddUserModel> users = new List<AddUserModel>();
+            if (response.IsSuccessStatusCode)   
+            {
+                users = response.Content.ReadAsAsync<List<AddUserModel>>().Result;
+              
+            }
+            ViewBag.Message = "Your contact page.";
+            ViewBag.users = users;
+            
+            
+
+            return View();
+        }
+
+    }
+}
